c07cf881680d364ae1b2bb95cfb62a1300d683b3
jaanjaanahy36 <jaanjaanahy36@gmail.com>
2026-09-09 21:14:02 +0500

    Update RAJPOOT key details in keylist.lua

--- keylist.lua
+++ keylist.lua
@@ -17,11 +17,11 @@ return {
         max_devices = 1,
         SLOT = "2"
     },
-    ["RAJPOOT"] = {
+    ["RAJPOOT60"] = {
         type = "DEMO",
-        expiry = "2026-10-10",
+        expiry = "2026-11-10",
         valid = true,
-        max_devices = 2,
+        max_devices = 1,
         SLOT = "3"
     },
     ["RAJPDISK"] = {
